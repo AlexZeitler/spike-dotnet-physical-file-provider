d26c851bde718d7bbf037bdbb590c9bb42946125
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-06-21 16:19:17 +0200

    feat: watch for file changes

--- PhysicalFileProviderLinux/Program.cs
+++ PhysicalFileProviderLinux/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+PhysicalFileProvider fileProvider;
+IChangeToken fileChangeToken;
+
+void WatchForFileChanges()
+{
+  fileChangeToken = fileProvider.Watch("*.*");
+  fileChangeToken.RegisterChangeCallback(Notify, "default");
+}
+
+void Notify(object state)
+{
+  Console.WriteLine("File change detected");
+  WatchForFileChanges();
+}
+
+fileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "./watch"));
+WatchForFileChanges();
+
+Console.WriteLine("Watching for file changes in ./watch");
+Console.WriteLine("Press any key to stop watching.");
+
+Console.ReadKey(false);
